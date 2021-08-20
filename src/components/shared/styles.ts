7182313bfd8971480eecf2c2a0e2bab6439869c1
Eungyeol <eungyeole@naver.com>
2021-08-20 11:07:31 +0900

    :memo: :: Color StoryBook Docs 작성

---
+++ src/components/shared/styles.ts
@@ -0,0 +1,20 @@
+export const color = {
+    primary : {
+        purple : '#9650FA',
+        black : '#212121',
+        white : '#FFFFFF',
+    },
+    secondary : {
+        yellow : '#FAF450',
+        indigo : '#684DF0'
+    }
+}
+
+export const typography = {
+    weight : {
+        light : '200',
+        regular : '300',
+        medium : '400',
+        bold : '500'
+    }
+}
