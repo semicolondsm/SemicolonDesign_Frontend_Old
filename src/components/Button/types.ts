e60599ada94d759d843e1552666267cad1979bc2
Sung Ye In <syi9397@naver.com>
2021-09-23 20:26:14 +0900

    ⚡️ :: 사용되는 타입 짜르기 및 붙이기

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -2,13 +2,19 @@ import { ReactElement, Component, MouseEvent } from 'react';
 import { colors } from '../shared/styles';
 
 export type SizeType = "sm" | "md" | "lg";
-export type FillStyleType = "default" | "purple" | "purpleLight" | "border" | "full";
+export type BackgroundNoneSupportFillStyleType = "default" | "purple";
+export type FillStyleType = "purpleLight" | "link" | "border" | "full" | BackgroundNoneSupportFillStyleType;
+export type BackgroundNoneFillStyleType = "bnDefault" | "bnPurple";
+export interface FillStyleObjectType {
+  fillStyle: FillStyleType,
+  background: boolean
+} 
 export type CursorType = "not-allowed" | "progress" | "pointer";
 
 export interface ButtonProps {
   leftIcon?: ReactElement<any, string | ((props: any) => ReactElement<any, any> | null) | (new (props: any) => Component<any, any, any>)>;
   rightIcon?: ReactElement<any, string | ((props: any) => ReactElement<any, any> | null) | (new (props: any) => Component<any, any, any>)>;
-  fill?: FillStyleType;
+  fill?: FillStyleType | FillStyleObjectType;
   size?: SizeType;
   className?: string;
   loading?: boolean;
@@ -23,7 +29,7 @@ export interface ButtonElementProps {
     paddingVertical: number;
     paddingHorizontal: number;
     size: SizeType;
-    fillStyle: FillStyleType;
+    fillStyle: FillStyleType | FillStyleObjectType;
     marginLeft: number;
     marginRight: number;
 };
@@ -31,13 +37,26 @@ export interface ButtonElementProps {
 export const Colors = {
   default: colors.grey100,
   defaultActive: colors.grey300,
+
   purple: colors.purple400,
   purpleActive: colors.purple500,
+
   purpleLight: colors.purple50,
   purpleLightActive: colors.purple100,
+
   border: colors.white,
   borderColor: colors.grey100,
   borderActive: colors.grey100,
+
+  link: colors.white,
+  linkActive: colors.white,
+
+  bnDefault: colors.white,
+  bnDefaultActive: colors.grey50,
+
+  bnPurple: colors.white,
+  bnPurpleActive: colors.purple50,
+
   full: colors.grey100
 };
 
@@ -46,5 +65,8 @@ export const FontColors = {
   purple: colors.white,
   purpleLight: colors.purple400,
   border: colors.grey700,
-  full: colors.grey700
+  link: colors.blue400,
+  full: colors.grey700,
+  bnDefault: colors.grey700,
+  bnPurple: colors.purple400
 }
