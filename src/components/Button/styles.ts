1b4be30ef9a9c671f468fb9cf899499c3b0613a0
Sung Ye In <syi9397@naver.com>
2021-09-23 14:01:14 +0900

    ⚡️ :: Button 종류에 대한 Css 수정 및 추가

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,45 +1,83 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-
-type CursorType = "not-allowed" | "progress" | "pointer";
-type BackgroundType = "#F5F5F5" | "#9650FA" | "#F0E6FF";
-
-interface ButtonElementProps {
-    cursor: CursorType;
-    background: BackgroundType;
-    width: number;
-    height: number;
-};
+import { colors } from '../shared/styles';
+import {
+    ButtonElementProps, 
+    SizeType, 
+    Colors, 
+    FillStyleType 
+} from './types';
 
 export const ButtonElement = styled.button<ButtonElementProps>`
+    border: none;
     cursor: ${(props) => props.cursor};
     background: ${(props) => props.background};
-    width: ${(props) => props.width}px;
-    height: ${(props) => props.height}px;
+    /* width: ${(props) => props.width}px;
+    height: ${(props) => props.height}px; */
+    margin-left: ${(props) => props.marginLeft}px;
+    margin-right: ${(props) => props.marginRight}px;
+    ${(props) => sizeToCssObject(props.size)}
+    ${(props) => colorToCssObject(props.fillStyle)}
 `;
 
 export const Large = css`
-
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 12px;
 `;
 
 export const Medium = css`
-
+    padding: 16px 67px;
+    border-radius: 12px;
 `;
 
 export const Small = css`
+    padding: 14px 16px;
+    border-radius: 4px;
+`;
 
+export const Default = css`
+    color: ${Colors.defaultColor};
+    &:hover {
+        background: ${Colors.defaultActive};
+    }
+
+    &:active {
+        background: ${Colors.defaultActive};
+    }
 `;
 
 export const Purple = css`
+    &:hover {
 
+    }
+
+    &:active {
+
+    }
 `;
 
 export const PurpleLight = css`
+    &:hover {
 
+    }
+
+    &:active {
+
+    }
 `;
 
 export const Border = css`
+    &:hover {
 
+    }
+
+    &:active {
+
+    }
 `;
 
 export const Link = css`
@@ -47,3 +85,29 @@ export const Link = css`
 `;
 
 // export const 
+
+function sizeToCssObject(size: SizeType) {
+    switch(size) {
+        case "sm":
+            return Small;
+        case "md":
+            return Medium;
+        case "lg":
+            return Large;
+        default:
+            return Medium;
+    }
+}
+
+function colorToCssObject(color: FillStyleType) {
+    switch(color) {
+        case "default":
+            return Default;
+        case "purple":
+            return Purple;
+        case "purpleLight":
+            return PurpleLight;
+        default:
+            return Default;
+    }
+}
