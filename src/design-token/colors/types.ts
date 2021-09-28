a0898fbf5055c56eb675a5b220473188535bdb89
Eungyeol <eungyeole@naver.com>
2021-09-28 11:59:48 +0900

    ⚡️ :: design-token 추가

---
+++ src/design-token/colors/types.ts
@@ -0,0 +1,20 @@
+const knownColorGroupNames = ['purple', 'grey', 'yellow', 'indigo', 'red', 'green', 'blue'] as const;
+export type KnownColorGroup = typeof knownColorGroupNames[number];
+type MakeTokenSet<TGroup extends KnownColorGroup, TLightness extends number[]> = `${TGroup}${TLightness[number]}`;
+
+type ColorToken = (
+    | 'white'
+    | MakeTokenSet<'grey', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    | MakeTokenSet<'purple', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    | MakeTokenSet<'yellow', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    | MakeTokenSet<'indigo', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    | MakeTokenSet<'red', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    | MakeTokenSet<'green', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    | MakeTokenSet<'blue', [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]>
+    
+)
+export type ColorScheme = Record<ColorToken, string>;
+
+export type ColorTheme = {
+    scheme: ColorScheme
+}
