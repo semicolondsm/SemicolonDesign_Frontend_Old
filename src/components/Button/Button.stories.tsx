5c3d27abcc1949d09cfa729e82824c0509dd4054
Sung Ye In <syi9397@naver.com>
2021-09-29 21:18:14 +0900

    ⚡️ :: 아예 기본 버튼

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,9 +1,3 @@
 import Button from "./index";
 export default { title: 'BasicButton' };
-export const Test = () => <Button
-    fill={{
-        background: false,
-        fillStyle: "default"
-    }}
-    size="sm"
->버튼</Button>;
+export const Test = () => <Button>버튼</Button>;
