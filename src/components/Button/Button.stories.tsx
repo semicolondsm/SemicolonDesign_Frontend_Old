8b42fba22f18ff8cd5ede50a55e13b1d14cf125c
Sung Ye In <syi9397@naver.com>
2021-09-29 21:17:37 +0900

    ⚡️ :: Default Button만 넣어둠

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,4 +1,9 @@
-import { Button } from "./Button";
+import Button from "./index";
 export default { title: 'BasicButton' };
-export const Test = () => <Button label="test"></Button> 
-export const Control = () => <Button label="test"></Button> 
+export const Test = () => <Button
+    fill={{
+        background: false,
+        fillStyle: "default"
+    }}
+    size="sm"
+>버튼</Button>;
