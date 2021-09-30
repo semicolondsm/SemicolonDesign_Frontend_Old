15dd94ed27be2a8ca5d60bda6f784e4132528752
Sung Ye In <syi9397@naver.com>
2021-09-30 19:39:52 +0900

    ⚡️ :: 함수로 color 가져오고 styled에 theme로 컬러 가져오기

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,18 @@
 import React, { FC } from 'react';
 import styled from '@emotion/styled';
+import Spinner from '../../static/svg/Spinner'; 
 import { css } from '@emotion/react';
+import { Botton } from '../typography';
 import { 
   Colors, 
   ButtonProps,
-  FontColors,
-  ActiveColors,
-  DisabledColors,
-  DisabledFontColors,
   ButtonElementProps,
 } from './types';
-import { Botton } from '../typography';
 import { 
   colorObjectToColorString,
+  fillStyleToColorString,
   isBackgroundNone
 } from './utils';
-import Spinner from '../../static/svg/Spinner';
 
 enum Cursor {
   DISABLED = "not-allowed",
@@ -55,10 +52,10 @@ export const ButtonElement = styled.button<ButtonElementProps>`
     align-items: center;
     border: none;
     cursor: ${(props) => props.cursor};
-    background: ${(props) => props.background};
+    background: ${(props) => props.theme.colors[props.background]};
     padding: ${(props) => `${props.paddingVertical}px ${props.paddingHorizontal}px`};
     border-radius: ${(props) => props.borderRadius}px;
-    border: 1px solid ${(props) => props.borderColor};
+    border: 1px solid ${(props) => props.theme.colors[props.borderColor]};
     
     ${(props) => props.isFull && css`
         width: 100%;
@@ -71,12 +68,12 @@ export const ButtonElement = styled.button<ButtonElementProps>`
 
     ${(props) => props.fillStyle === "link" && css`
         &:hover, &:active {
-            text-decoration: underline 1px solid ${FontColors.link};
+            text-decoration: underline 1px solid ${props.theme.colors[props.color]};
         }
     `}
 
     &:hover, &:active {
-        background: ${(props) => props.activeBackground};
+        background: ${(props) => props.theme.colors[props.activeBackground]};
     }
 
     & .semicolon-button-typography {
@@ -100,9 +97,17 @@ const Button: FC<ButtonProps> = (props = initialProps) => {
 
   const cursorType = disabled ? "DISABLED" : loading ? "LOADING" : "DEFAULT";
   const colorString = colorObjectToColorString(fill, true);
-  const BackgroundColor = disabled ? DisabledColors[colorString] : Colors[colorString];
-  const BackgroundActiveColor =  disabled ? DisabledColors[colorString] : ActiveColors[colorString];
-  const FontColor = disabled ? DisabledFontColors[colorString] : FontColors[colorString];
+  const BackgroundColor = disabled ? 
+    fillStyleToColorString(colorString, "diabled") : 
+    fillStyleToColorString(colorString, "default");
+
+  const BackgroundActiveColor = disabled ? 
+    fillStyleToColorString(colorString, "diabled") : 
+    fillStyleToColorString(colorString, "active");
+
+  const FontColor = disabled ? 
+    fillStyleToColorString(colorString, "disabledFont") :
+    fillStyleToColorString(colorString, "font");
 
   const styledProps = {
     cursor: Cursor[cursorType],
@@ -114,6 +119,7 @@ const Button: FC<ButtonProps> = (props = initialProps) => {
     borderColor: colorString === "border" ? Colors["borderColor"] : BackgroundColor,
     size: size || "md",
     fillStyle: colorString || "default",
+    color: FontColor,
     isFull: typeof fill !== "string" && fill?.full === true,
     marginLeft: leftIcon ? 6 : 0,
     marginRight: loading || rightIcon ? 6 : 0,
