abfd56d74d7648406977e6eda0db609c818b8d90
Eungyeol <eungyeole@naver.com>
2021-09-30 16:40:53 +0900

    ⚡️ :: storybook theming 적용

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Semicolon Design',
+//   brandUrl: 'https://<url>>',
+//   brandImage: 'https://<url>',
+});
